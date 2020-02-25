2f2aa6b790ccc636c94ed9b4dab2419b59738dcc
Francisco Montero <fmonteroponcedeleon@gmail.com>
2020-02-25 15:34:59 -0300

    Total Monthly Amount Per Customer report was implemented.

--- src/app/components/reports/total-monthly-customer/total-monthly-customer.component.ts
+++ src/app/components/reports/total-monthly-customer/total-monthly-customer.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { BusinessService } from 'src/app/services/business.service';
 
 import { Customer } from 'src/app/models/customer';
+import { Service } from 'src/app/models/service';
 import { ServiceCustomer } from 'src/app/models/service-customer';
 import { TotalMonthlyCustomer } from 'src/app/models/total-monthly-customer';
 
@@ -14,17 +15,24 @@ import { TotalMonthlyCustomer } from 'src/app/models/total-monthly-customer';
 export class TotalMonthlyCustomerComponent implements OnInit {
 
   public customers: Customer[] = [];
+  public services: Service[] = [];
   public servicesCustomers: ServiceCustomer[] = [];
   public totalMonthlyCustomers: TotalMonthlyCustomer[] = [];
+
+  public isLoadingCustomers = false;
+  public isLoadingServices = false;
+  public isLoadingServiceCustomers = false;
 
   constructor(private businessService: BusinessService) { }
 
   ngOnInit() {
     this.getCustomers();
+    this.getServices();
     this.getServiceCustomer();
   }
 
   getCustomers(): void {
+    this.isLoadingCustomers = true;
     this.businessService.getCustomers().subscribe(
       (serviceResult: any[]) => {
         this.customers = serviceResult.map((c: any) => {
@@ -38,14 +46,44 @@ export class TotalMonthlyCustomerComponent implements OnInit {
           };
         });
         console.log('customers', this.customers);
+        this.isLoadingCustomers = false;
+        if (this.isLoadingFinished()) {
+          this.calculateTotalMonthlyAmountPerCustomer();
+        }
       },
       error => {
         // TODO: Handling error
+        this.isLoadingCustomers = false;
+      }
+    );
+  }
+
+  getServices(): void {
+    this.isLoadingServices = true;
+    this.businessService.getServices().subscribe(
+      (serviceResult: any[]) => {
+        this.services = serviceResult.map((s: any) => {
+          return {
+            id: s.id,
+            name: s.nombre,
+            monthlyFee: s.cuotaMensual
+          };
+        });
+        console.log('services', this.services);
+        this.isLoadingServices = false;
+        if (this.isLoadingFinished()) {
+          this.calculateTotalMonthlyAmountPerCustomer();
+        }
+      },
+      error => {
+        // TODO: Handling error
+        this.isLoadingServices = false;
       }
     );
   }
 
   getServiceCustomer(): void {
+    this.isLoadingServiceCustomers = true;
     this.businessService.getServiceCustomer().subscribe(
       (serviceResultGet: any) => {
         this.servicesCustomers = serviceResultGet.map((sc: any) => {
@@ -57,16 +95,54 @@ export class TotalMonthlyCustomerComponent implements OnInit {
           };
         });
         console.log('serviceCustomers', this.servicesCustomers);
+        this.isLoadingServiceCustomers = false;
+        if (this.isLoadingFinished()) {
+          this.calculateTotalMonthlyAmountPerCustomer();
+        }
       },
       error => {
         // TODO: Handling error
+        this.isLoadingServiceCustomers = false;
       }
     );
   }
 
-  calculateTotalMonthlyAmountPerCustomer(): void {
-    
-    
+  isLoadingFinished(): boolean {
+    return !this.isLoadingCustomers && !this.isLoadingServices
+      && !this.isLoadingServiceCustomers ? true : false;
+  }
 
+  calculateTotalMonthlyAmountPerCustomer(): void {
+    console.log('calculateTotalMonthlyAmountPerCustomer');
+    for (let i = 0; i < this.customers.length; i++) {
+      for (let j = 0; j < this.servicesCustomers.length; j++) {
+        if (this.customers[i].id === this.servicesCustomers[j].customerId) {
+          let serviceCustomer = this.servicesCustomers[j];
+          let service = this.services.find(s => s.id === serviceCustomer.serviceId);
+          let serviceMonthlyFee = !!service ? service.monthlyFee : 0;
+
+          // TODO: Isolate with new function!
+          let totalMonthlyCustomer = this.totalMonthlyCustomers.find(
+            tmc => tmc.customerId === serviceCustomer.customerId
+              && tmc.month === new Date(serviceCustomer.dateOfAssociation).getMonth() + 1
+              && tmc.year === new Date(serviceCustomer.dateOfAssociation).getFullYear());
+
+          if (!!totalMonthlyCustomer) {
+            totalMonthlyCustomer.totalAmount = totalMonthlyCustomer.totalAmount + serviceMonthlyFee;
+          } else {
+            totalMonthlyCustomer = new TotalMonthlyCustomer();
+            totalMonthlyCustomer.customerId = this.customers[i].id;
+            totalMonthlyCustomer.fullName = this.customers[i].fullName;
+            totalMonthlyCustomer.documentNumber = this.customers[i].documentNumber;
+            totalMonthlyCustomer.totalAmount = serviceMonthlyFee;
+            console.log('serviceCustomer', serviceCustomer);
+            totalMonthlyCustomer.month = new Date(serviceCustomer.dateOfAssociation).getMonth() + 1;
+            totalMonthlyCustomer.year = new Date(serviceCustomer.dateOfAssociation).getFullYear();
+            this.totalMonthlyCustomers.push(totalMonthlyCustomer);
+          }
+        }
+      }
+    }
+    console.log('totalMonthlyCustomers', this.totalMonthlyCustomers);
   }
 }
